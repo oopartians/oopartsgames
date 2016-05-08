848362754a74d194ec395af4567747969dc347bd
Cona <hyeonso5@gmail.com>
2016-05-08 13:41:08 +0000

    sample login page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Ooparts games' });
 });
 
+router.get('/test', function(req, res, next) {
+  res.render('signin', { title: 'Ooparts games' });
+});
+
 module.exports = router;
