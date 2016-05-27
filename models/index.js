b21a418df57e5f0e31583859d9d53f6936090187
Hyeonseok Oh <hyeonso5@gmail.com>
2016-05-27 07:02:09 +0000

    Remove comment because environment variable should be applied.

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@
 var fs        = require("fs");
 var path      = require("path");
 var Sequelize = require("sequelize");
-var env       = /*process.env.NODE_ENV || */"production";
+var env       = process.env.NODE_ENV || "production";
 var config    = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
 var sequelize = new Sequelize(config.database, config.username, config.password, config);
 var db        = {};
